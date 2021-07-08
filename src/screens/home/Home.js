2b00fb14a21c0093494241abeea94e5b2923d75d
Rahul Singh Raghav <officialrahulsr@gmail.com>
2021-07-08 12:24:53 +0530

    Added Movie name filter on home page

--- src/screens/home/Home.js
+++ src/screens/home/Home.js
@@ -3,9 +3,16 @@ import "./Home.css";
 import Header from '../../common/header/Header';
 import { withStyles } from '@material-ui/core/styles';
 import moviesData from '../../common/movieData';
-import GridList from '@material-ui/core/GridList';
-import GridListTile from '@material-ui/core/GridListTile';
-import GridListTileBar from '@material-ui/core/GridListTileBar';
+import ImageList from '@material-ui/core/ImageList';
+import ImageListItem from '@material-ui/core/ImageListItem';
+import ImageListItemBar from '@material-ui/core/ImageListItemBar';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import FormControl from '@material-ui/core/FormControl';
+import Typography from '@material-ui/core/Typography';
+import InputLabel from '@material-ui/core/InputLabel';
+import Input from '@material-ui/core/Input';
+
 
 const styles = theme => ({
     root: {
@@ -18,31 +25,90 @@ const styles = theme => ({
         padding: '8px',
         fontSize: '1rem'
     },
-    gridListUpcomingMovies: {
+    imageListUpcomingMovies: {
         flexWrap: 'nowrap',
         transform: 'translateZ(0)',
         width: '100%'
-    }
+    },
+    imageListMain: {
+        transform: 'translateZ(0)',
+        cursor: 'pointer'
+    },
+    formControl: {
+        margin: theme.spacing.unit,
+        minWidth: 240,
+        maxWidth: 240
+     },
+     title: {
+        color: theme.palette.primary.light,
+     }
+
  });
  
 
 class Home extends Component {
+
+    constructor(){
+        super();
+        this.state = {
+            movieName: "",
+
+        }
+    }
+
+    movieNameChangeHandler = (event) =>{
+        this.setState({movieName:event.target.value});
+        
+        
+    }
+
     render(){
         const { classes } = this.props;
         return(
             <div>
                 <Header />
-                    <div className = {classes.upcomingMoviesHeading}>
-                        <span>Upcoming Movies</span>
+                <div className = {classes.upcomingMoviesHeading}>
+                    <span>Upcoming Movies</span>
+                </div>
+                <ImageList cols={5} className = {classes.imageListUpcomingMovies}>
+                    {moviesData.map(movie => (
+                        <ImageListItem key = {movie.id}>
+                            <img className = "movie-poster" src= {movie.poster_url} alt = {movie.title}/>
+                            <ImageListItemBar title = {movie.title}/>
+                        </ImageListItem>
+                    ))}
+                </ImageList>
+
+                <div className="flex-container">
+                    <div className="left">
+                        <ImageList rowHeight={350} cols={4} className={classes.imageListMain}>
+                            {moviesData.map(movie => (
+                                <ImageListItem className="released-movie-imagelist-item" key={"grid" + movie.id}>
+                                    <img src={movie.poster_url} className="movie-poster" alt={movie.title} />
+                                    <ImageListItemBar
+                                        title={movie.title}
+                                        subtitle={<span>Release Date: {new Date(movie.release_date).toDateString()}</span>}
+                                    />
+                                </ImageListItem>
+                            ))}
+                        </ImageList>
                     </div>
-                    <GridList cols={5} className = {classes.gridListUpcomingMovies}>
-                        {moviesData.map(movie => (
-                            <GridListTile key = {movie.id}>
-                                <img className = "movie-poster" src= {movie.poster_url} alt = {movie.title}/>
-                                <GridListTileBar title = {movie.title}/>
-                            </GridListTile>
-                        ))}
-                    </GridList>
+                    <div className="right">
+                        <Card>
+                            <CardContent>
+                                <FormControl className = {classes.formControl}>
+                                    <Typography className = {classes.title} coloe= "textSecondary">
+                                        FIND MOVIES BY:
+                                    </Typography>
+                                </FormControl>
+                                <FormControl className = {classes.formControl}>
+                                    <InputLabel htmlFor="movieName" > Movie Name </InputLabel>
+                                    <Input id = "movieName" onChange = {this.movieNameChangeHandler} />
+                                </FormControl>
+                            </CardContent>
+                        </Card>
+                    </div>
+                </div>
             </div>
         )
     }
