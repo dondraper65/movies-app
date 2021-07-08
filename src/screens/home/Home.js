fc9ba7f856ceb3975249c3de3ec7e0d55cdb1b72
Rahul Singh Raghav <officialrahulsr@gmail.com>
2021-07-08 13:23:35 +0530

    Added release date start and end filters in Home page

--- src/screens/home/Home.js
+++ src/screens/home/Home.js
@@ -18,6 +18,8 @@ import Select from '@material-ui/core/Select';
 import MenuItem from '@material-ui/core/MenuItem';
 import Checkbox from '@material-ui/core/Checkbox';
 import ListItemText from '@material-ui/core/ListItemText';
+import TextField from '@material-ui/core/TextField';
+
 
 
 
@@ -114,6 +116,8 @@ class Home extends Component {
                     <div className="right">
                         <Card>
                             <CardContent>
+
+
                                 <FormControl className = {classes.formControl}>
                                     <Typography className = {classes.title} coloe= "textSecondary">
                                         FIND MOVIES BY:
@@ -123,6 +127,8 @@ class Home extends Component {
                                     <InputLabel htmlFor="movieName" > Movie Name </InputLabel>
                                     <Input id = "movieName" onChange = {this.movieNameChangeHandler} />
                                 </FormControl>
+
+
                                 <FormControl className = {classes.formControl}>
                                     <InputLabel htmlFor = "select-multiple-checkbox-genre">Genre</InputLabel>
                                     < Select multiple input= {<Input id = "select-multiple-checkbox-genre" />} renderValue = {selected => selected.join(',')} value = {this.state.genres} onChange = {this.genreSelectHandler}>
@@ -155,6 +161,16 @@ class Home extends Component {
                                         ))}
                                     </Select>
                                 </FormControl>
+
+                                <FormControl className = {classes.formControl}>
+                                    <TextField id = "releaseDateStart" label = "Release Date Start" type ="date" defaultValue = ""  InputLabelProps = {{shrink:true}}></TextField>
+                                </FormControl>
+
+                                <FormControl className = {classes.formControl}>
+                                    <TextField id = "releaseDateEnd" label = "Release Date End" type ="date" defaultValue = ""  InputLabelProps = {{shrink:true}}></TextField>
+                                </FormControl>
+
+
                             </CardContent>
                         </Card>
                     </div>
