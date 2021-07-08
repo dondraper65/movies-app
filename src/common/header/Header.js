6f0bd0939adf127cfd0ce6973076379a737a4cd2
Rahul Singh Raghav <officialrahulsr@gmail.com>
2021-07-08 18:21:18 +0530

    Added BookShow Component and Book Show button on header of details page

--- src/common/header/Header.js
+++ src/common/header/Header.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
 import "./Header.css";
 import Button from '@material-ui/core/Button';
 import logo from "../../assets/logo.svg"
@@ -11,6 +12,7 @@ import FormControl from '@material-ui/core/FormControl';
 import InputLabel from '@material-ui/core/InputLabel';
 import PropTypes from 'prop-types';
 import FormHelperText from '@material-ui/core/FormHelperText';
+import BookShow from '../../screens/bookshow/BookShow';
 
 const customStyles = {
     content: {
@@ -111,6 +113,13 @@ class Header extends Component {
         this.setState({contact: e.target.value})
     }
 
+    bookshowHandler = () =>{
+        ReactDOM.render(
+            <BookShow />,
+          document.getElementById('root')
+        );
+    }
+
 
 
 
@@ -124,6 +133,14 @@ class Header extends Component {
                             Login
                         </Button>
                     </div>
+                    {this.props.showBookShowButton ==='true' ?
+                    <div className = "bookshow-button">
+                        <Button variant="contained" color="primary" onClick = {this.bookshowHandler}>
+                            Book Show
+                        </Button>
+                    </div> : ""
+                    }       
+
                 </header>
                 <Modal ariaHideApp={false} isOpen= {this.state.modalIsOpen} contentLabel = "Login" onRequestClose = {this.closeModalHandler} style={customStyles}>
                     <Tabs className = "tabs" value = {this.state.value} onChange = {this.tabChangeHandler}  >
